80b5667249a7a969f392ba1bd38a8f8dfd8d8b4e
yamelsenih <ysenih@erpya.com>
2024-02-06 18:28:52 +0000

    Deploying to gh-pages from @ adempiere/adempiere-site@a849baa8cfe6ccb26e569a7e04edab621791bda2 🚀

---
+++ assets/js/1500.31f2a876.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[1500],{21500:(e,s,b)=>{b.r(s)}}]);
---
+++ assets/js/7048.21a867ff.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[7048],{57048:(e,s,t)=>{t.r(s),t.d(s,{updatePageview:()=>b});var a=t(3192);const b=()=>(0,a.k)({serverURL:"https://<to-be-defined>"})}}]);
